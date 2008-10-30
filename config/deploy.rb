4d008d3269a1778063078492a36efc890fda539a
vann <vann@innerfusion.net>
2008-10-30 11:37:27 -0500

    added extra comments to deploy script

--- config/deploy.rb
+++ config/deploy.rb
@@ -3,9 +3,11 @@
 
 # the location of the project
 FROM = "~/location/of/your/project"
-# the ip or hostname of the server
+# the ip or hostname of the server, can use the following conventions for this variable:
+#   - server_name # => if you have server_name setup in ~/.ssh/config
+#   - username@host 
 SERVER = "project_hostname"
-# the location on the server
+# the location on the server don't forget to append the slash/
 TO = "~/location/of/project/on/server"
 
 puts("Starting deployment...")

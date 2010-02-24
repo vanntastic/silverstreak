d2c6b18fed4a86e53fdecc84d1d6a213e71910b7
vann <vann@innerfusion.net>
2010-02-24 11:58:06 -0600

    added truncate function

--- _helpers.php
+++ _helpers.php
@@ -304,4 +304,21 @@
     echo tag($name,$content,$attrs);
   }
   
+  // Original PHP code by Chirp Internet: www.chirp.com.au
+
+  function truncate($string, $limit, $break=".", $pad="...")
+  {
+    // return with no change if string is shorter than $limit
+    if(strlen($string) <= $limit) return $string;
+
+    // is $break present between $limit and the end of the string?
+    if(false !== ($breakpoint = strpos($string, $break, $limit))) {
+      if($breakpoint < strlen($string) - 1) {
+        $string = substr($string, 0, $breakpoint) . $pad;
+      }
+    }
+
+    return $string;
+  }
+  
 ?>
